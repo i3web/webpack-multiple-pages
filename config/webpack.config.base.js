a4a7a906e20da49c5a9f315a54d5fb9f53835373
louhangfei <itnote@qq.com>
2018-03-11 16:42:25 +0800

    init

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -10,8 +10,17 @@ let Entries = {}
 
 config.HTMLDirs.forEach((page,index) => {
     const htmlPlugin = new HTMLWebpackPlugin({
+        //生成的文件名
         filename: `${page}.html`,
+        //根据自己的指定的模板文件来生成特定的 html 文件
         template: path.resolve(__dirname, `../app/html/${page}.html`),
+        //生成html的title
+        title:'hello'+page,
+        //生成的favicon.ico
+        //给生成的 js 文件一个独特的 hash 值,是文件名后带？的那个hash,output中的[chunkhash]是文件名中带hash
+        hash:true,
+        //chunks 选项的作用主要是针对多入口(entry)文件。当你有多个入口文件的时候，对应就会生成多个编译后的 js 文件。那么 chunks 选项就可以决定是否都使用这些生成的 js 文件。
+        //chunks 默认会在生成的 html 文件中引用所有的 js 文件，当然你也可以指定引入哪些特定的文件。
         chunks: [page, 'commons'],
     });
     HTMLPlugins.push(htmlPlugin);
